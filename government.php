b4a6266884aad89671b3e248db08404008743a63
Melanie27 <melaniemcganney@gmail.com>
2013-11-20 11:49:22 -0800

    government

--- government.php
+++ government.php
@@ -40,38 +40,142 @@
 		  				
 						
 						<div class="container">
-							<section class="three">
+							<section class="three overview">
 								<div class="row over2">
-									<section class="col-lg-12 col-md-12 news-columns" style="padding-left:0px;">
-									
+									<section class="col-lg-12 col-md-12" style="padding-left:0px;">
 										
-										<div class="row"  >
-											
-											<section class="col-lg-4 col-md-4 col-sm-12 col-xs-12 hidden-md hidden-lg latest-news">		
-											<?php include "_/components/php/latest-news.php"; ?>
-											</section><!--Latest News-->
-											
-											<section class="col-lg-4 col-md-4 col-sm-12 col-xs-12 cols first-col">	
-												<?php include "_/components/php/press-room.php"; ?>	
-											</section><!--Press Room-->	
-											
-											<section class="col-lg-4 col-md-4 col-sm-12 col-xs-12 cols second-col ">
-												<?php include "_/components/php/in-the-news.php"; ?>
-											</section><!--In the News-->
-												
-											<section class="col-lg-4 col-md-4 col-sm-12 col-xs-12 cols third-col">	
-													<?php include "_/components/php/awards-honors.php"; ?>
-											</section><!--Awards-->		
-											
-											<section class ="hidden-lg hidden-md">
-												<?php include "_/components/php/events-mobile.php"; ?>
-											</section>	
+										
+										<div class="row overview-top">
+											<section class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
+												<h1>Extended Stay Lodging for the Federal Government</h1>
+												<h2>For over 50 years, Oakwood Worldwide has given those who serve America a place they could call home.</h2>
+												<p>As the first, largest and leading provider of extended stay lodging (ESL) solutions to the federal government, Oakwood offers a welcoming environment for government employees, with convenient locations, and access to various transportation options.</p>
+											</section><!--end two thirds-->
+											<section class="col-lg-4 col-md-4 hidden-sm hidden-xs">
+												<h2>Contact Us</h2>
+												<div class="row">
+													<section class="col-lg-6 col-md-6">
+														<h3>US & Canada: </h3>
+														<p class="phone">1-888-268-9998</p>
+														<p class="email">Email Us</p>
+													</section>
+													<section class="col-lg-6 col-md-6">
+														<h3>Everywhere Else: </h3>
+														<p class="phone">1-757-648-7171</p>
+													</section>
+												</div><!--end second nested row-->
+											</section><!--end contact us-->
 										</div><!--end nested row-->
+										
+										<div class="row overview-sections solutions-columns">
+										<h2>Proudly Serving:</h2>
+											<section class="col-lg-4 col-md-4 col-sm-6 col-xm-12">
+												<div class="solutions military">
+													<a href="#" class="industry-sections">
+														<img src="images/government/overview-01.jpg" alt="Woman Texting" class="img-responsive" />
+														<header class="industries">
+															<h3>Military</h3>
+															<div class="clearfix"></div>
+														</header>
+													</a>
+													<section class="industry-text">	
+														<p class="consult-desc">Lorem ipsum in dui mauris. Vivamus hendrerit arcu sed erat molestie vehicula.</p>
+														<a href="#" class="orange-link">Learn More--></a>
+													</section>
+												</div><!--end military-->
+											</section>
+											<section class="col-lg-4 col-md-4 col-sm-6 col-xm-12">
+												<div class="solutions military">
+													<a href="#" class="industry-sections">
+														<img src="images/government/overview-02.jpg" alt="Woman Texting" class="img-responsive" />
+														<header class="industries">
+															<h3>Federal Civilians</h3>
+															<div class="clearfix"></div>
+														</header>
+													</a>
+													<section class="industry-text">	
+														<p class="consult-desc">Lorem ipsum in dui mauris. Vivamus hendrerit arcu sed erat molestie vehicula.</p>
+														<a href="#" class="orange-link">Learn More--></a>
+													</section>
+												</div><!--end military-->
+											</section>
+											<section class="col-lg-4 col-md-4 col-sm-6 col-xm-12">
+												<div class="solutions military">
+													<a href="#" class="industry-sections">
+														<img src="images/government/overview-03.jpg" alt="Woman Texting" class="img-responsive" />
+														<header class="industries">
+															<h3>Government Contractors</h3>
+															<div class="clearfix"></div>
+														</header>
+													</a>
+													<section class="industry-text">	
+														<p class="consult-desc">Lorem ipsum in dui mauris. Vivamus hendrerit arcu sed erat molestie vehicula.</p>
+														<a href="#" class="orange-link">Learn More--></a>
+													</section>
+												</div><!--end military-->
+											</section>
+										</div>
+										
+										<div class="row hidden-sm hidden-xs overview-gsa">
+											<section class="col-lg-11 col-md-11">
+												<h2>Oakwood Worldwide is an approved GSA Schedule 48 vendor and proud to be an extended stay lodging provider for FedRooms with rates that fall within TDY and most PCS per diems.</h2>
+												<a href="#" class="orange-link col-lg-offset-5 col-md-offset-5">Learn More--></a>
+											</section>
+										</div><!--end gsa-->
+										
+										<div class="row">
+											<section>
+												<h1>Why does the Federal Government Prefer Oakwood?</h1>
+												<!-- Nav tabs -->
+												<ul class="nav nav-tabs">
+													<li><a href="#space" data-toggle="tab">Space</a></li>
+													<li><a href="#service" data-toggle="tab">Service</a></li>
+													<li><a href="#simplicity" data-toggle="tab">Simplicity</a></li>
+													<li><a href="#security" data-toggle="tab">Security</a></li>
+												</ul>
+												
+												<!-- Tab panes -->
+												<div class="tab-content">
+													<div class="tab-pane active" id="space">
+														<ul>
+															<li>One point of contact for the procurement office, 24/7/365 support for the traveler.</li>
+															<li>Global presense and local service, through 75+ offices in the U.S., Europe, and Asia Pacific.</li>
+															<li>Dedicared military specialists with the speed, flexibility, and expertise to meet your mission-critical demands.</li>
+														</ul>
+													</div>
+													<div class="tab-pane" id="service">
+														<ul>
+															<li>Service</li>
+															<li>Service</li>
+															<li>Service</li>
+														</ul>
+
+													</div>
+													<div class="tab-pane" id="simplicity">
+														<ul>
+															<li>Simplicity</li>
+															<li>Simplicity</li>
+															<li>Simplicity</li>
+														</ul>
+													</div>
+													<div class="tab-pane" id="security">
+														<ul>
+															<li>Security</li>
+															<li>Security</li>
+															<li>Security</li>
+														</ul>
+													</div>
+												</div>
+											
+											</section>	
+										</div><!--end tabbed section-->
+										
+									
 										
 									</section>
 								</div><!--row-->
-							</section><!--three-->
-						</div>
+							</section><!--three overview-->
+						</div><!--end container-->
 					
 				
 				<div class="content row">
