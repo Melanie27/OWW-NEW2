20249d3ff113b265a8f383467d536449b6895ef5
Melanie27 <melaniemcganney@gmail.com>
2013-12-05 10:19:36 -0800

    global services phone

--- global-services.php
+++ global-services.php
@@ -42,15 +42,15 @@
 						
 							<section class="three container">
 								<div class="row">
-									<section class="col-lg-12 col-md-12">
+									<section class="col-lg-12 col-md-12 global-intro">
 										<h1>Intro Placeholder Text</h1>
 										<h2>Our service model centers around our front-line accountability.</h2>
-										<p>Our philosophy is about finding solutions, not filling existing or vacant apartments — allowing us the flexibility to place you wherever you need to be. We remove your critical resources from the day-to-day operations of housing management, allowing you and your guests to focus on your work, not ours.</p>
+										<p>Our philosophy is about finding solutions, not filling existing or vacant apartments - allowing us the flexibility to place you wherever you need to be. We remove your critical resources from the day-to-day operations of housing management, allowing you and your guests to focus on your work, not ours.</p>
 									</section>
 								</div>
 								<hr>
 								<div class="row">
-									<section class="col-lg-12 col-md-12">
+									<section class="col-lg-12 col-md-12 global-reach">
 										<h1>Global Reach</h1>
 										<h2>Wherever you need to go, Oakwood Worldwide is there.</h2>
 									</section>
@@ -80,31 +80,78 @@
 								</div>			
 							</section><!--three-->
 						
-					
+				<hr>	
 				
 							<div class="row">
 								<div class="col-lg-12">
-									<section class="four hidden-sm">
+									<section class="four hidden-sm hidden-xs">
 									</section>
 								</div>
 							</div><!--content 4-->
 				
 				
 							<section class="five container">
+								
 								<div class="row">
-									<section class="col-lg-12 col-md-12">
+									<section class="col-lg-12 col-md-12 experience">
 										<h1>Experience and Expertise</h1>
-										<h2>Oakwood Worldwide’s commitment to providing seamless housing solutions extends far past our physical properties.</h2>
+										<h2>Oakwood Worldwide's commitment to providing seamless housing solutions extends far past our physical properties.</h2>
 										
 									</section>
 								</div>
 								
 								<div class="row">
-								<?php include "_/components/php/industry-solutions.php"; ?>
+									<section class="col-lg-4 col-md-4 col-sm-6 col-xs-12">		
+										<div class="solutions clearfix" id="client-services">
+											<a href="#">
+												<img src="images/business-solutions/consulting.jpg" alt="Woman Texting" class="img-responsive" />
+												<header class="industries">
+													<h3>Client Services</h3>
+													<div class="clearfix"></div>
+												</header>
+											</a>
+											<section class="industry-text">	
+												<p class="consult-desc">Our dedicated team of Associates work to provide you with a seamless experience.</p>	
+											</section>
+										</div>
+									</section>	
+									<section class="col-lg-4 col-md-4 col-sm-6 col-xs-12">		
+										<div class="solutions" id="technologies">
+											<a href="#">
+												<img src="images/business-solutions/government.jpg" alt="Woman Texting" class="img-responsive" />
+												<header class="industries">
+													<h3>Technologies</h3>
+													<div class="clearfix"></div>
+												</header>
+											</a>
+											<section class="industry-text">	
+												<p class="consult-desc">Proprietary technologies enrich your experience from start to end, providing the analytics, access and ease you need to manage your housing programs efficiently.</p>	
+											</section>
+										</div>
+									</section>	
+									<section class="col-lg-4 col-md-4 col-sm-6 col-xs-12">		
+										<div class="solutions" id="safety-security">
+											<a href="#">
+												<img src="images/business-solutions/insurance.jpg" alt="Woman Texting" class="img-responsive" />
+												<header class="industries">
+													<h3>Safety and Security</h3>
+													<div class="clearfix"></div>
+												</header>
+											</a>
+											<section class="industry-text">	
+												<p class="consult-desc">Our commitment to duty of care and crisis management ensures that risks are minimized, your guests are safe and your experience always exceeds expectations.</p>	
+											</section>
+										</div>
+									</section>	
 								</div>
-								
 							</section><!--content 5-->
   				
+							<div class="row">
+								<div class="col-lg-12">
+									<section class="six hidden-sm hidden-xs">
+									</section>
+								</div>
+							</div><!--content 6-->
   				
 				
 				<?php include "_/components/php/footer.php"; ?>	
