d1632b369e2e76f35fcda0cfd296abb1c1407b48
Melanie27 <melaniemcganney@gmail.com>
2013-12-06 13:41:44 -0800

    leadership ipad landscape

--- executive-leadership.php
+++ executive-leadership.php
@@ -51,10 +51,10 @@
 										<div class="row">
 											<section class="col-lg-8 col-md-8">
 												<div class="row">
-													<section class="col-lg-4 col-xs-6 pull-left">
+													<section class="col-lg-4 col-md-3 col-xs-6 pull-left">
 														<img src="images/vision-leadership/_players/howard-ruby.jpg" alt="howard ruby" class="img-responsive">
 													</section>
-													<section class="col-lg-8 no-left-padding">
+													<section class="col-lg-8 col-md-9 no-left-padding">
 														<p class="executive-text">Sed vamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna diam porttitor mauris, quis sollicitudin sapien justo in libero. Vestibulum mollis mauris enim. Morbi euismod magna ac lorem rutrum elementum. Donec viverra auctor lobortis. Pellentesque eu est a nulla placerat dignissim. Morbi a enim in magna semper bibendum. Etiam scelerisque, nunc ac egestas consequat.</p>
 														<p class="executive-text">Quisque eget odio ac lectus vestibulum faucibus eget in metus. In pellentesque faucibus vestibulum. Nulla at nulla justo, eget luctus tortor. Nulla facilisi. Duis aliquet egestas purus in blandit. Curabitur vulputate, ligula lacinia scelerisque tempor, lacus lacus ornare ante, ac egestas est urna sit amet arcu. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed molestie augue sit amet leo consequat posuere. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Proin vel ante a orci tempus eleifend ut et mag.
 												</p>
