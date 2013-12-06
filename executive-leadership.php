64091fe31ff157b0c42b15d3780f620bbf07e84d
Melanie27 <melaniemcganney@gmail.com>
2013-12-06 14:20:56 -0800

    leadership phone

--- executive-leadership.php
+++ executive-leadership.php
@@ -45,13 +45,16 @@
 									<section class="col-lg-12 col-md-12 col-xs-12 no-left-padding">
 										<div class="row">
 											<section class="col-lg-12 col-md-12 clearfix">	
-												<h1>Howard Ruby <span class="title">Chairman, Founder and COO</span> </h1>
+												
+												
+													<h1>Howard Ruby <span class="title hidden-xs">Chairman, Founder and COO</span> </h1>
+													<h3 class="hidden-lg hidden-md hidden-sm">Chairman, Founder and COO</h3>
 											</section>	
 										</div>	
 										<div class="row">
 											<section class="col-lg-8 col-md-8">
 												<div class="row">
-													<section class="col-lg-4 col-md-3 col-xs-6 pull-left">
+													<section class="col-lg-4 col-md-3 col-sm-3 col-xs-6 pull-left">
 														<img src="images/vision-leadership/_players/howard-ruby.jpg" alt="howard ruby" class="img-responsive">
 													</section>
 													<section class="col-lg-8 col-md-9 no-left-padding">
@@ -61,58 +64,78 @@
 														<p class="executive-text">Consectetur adipiscing elit. Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna diam porttitor mauris, quis sollicitudin sapien justo in libero. Vestibulum mollis mauris enim. Morbi euismod magna ac lorem rutrum elementum. </p>
 														<p class="executive-text">Donec viverra auctor lobortis. Pellentesque eu est a nulla placerat dignissim. Morbi a enim in magna semper bibendum. Etiam scelerisque, nunc ac egestas consequat, odio nibh euismod nulla, eget auctor orci nibh vel nisi. Aliquam erat volutpat. Mauris vel neque sit amet nunc gravida congue sed sit amet purus. Quisque lacus quam, egestas ac tincidunt a, lacinia vel velit. Aenean facilisis nulla vitae urna tincidunt congue sed ut dui. Morbi malesuada nulla nec purus convallis consequat. Vivamus id mollis quam. Morbi ac commodo nulla. In condimentum orci id nisl volutpat bibendum. 
 														</p>
-														<p class="executive-text">Quisque commodo hendrerit lorem quis egestas. Maecenas quis tortor arcu. Vivamus rutrum nunc non neque consectetur quis placerat neque lobortis. Nam vestibulum, arcu sodales feugiat consectetur, nisl orci bibendum elit, eu euismod magna sapien ut nibh. Donec semper quam scelerisque tortor dictum gravida. In hac habitasse platea dictumst. Nam pulvinar, odio sed rhoncus suscipit, sem diam ultrices mauris, eu consequat purus metus eu velit. Proin metus odio, aliquam eget molestie nec, gravida ut sapien. Phasellus quis est sed turpis sollicitudin sed eu odio. Praesent eget neque.</p>
+														<p class="executive-text last">Quisque commodo hendrerit lorem quis egestas. Maecenas quis tortor arcu. Vivamus rutrum nunc non neque consectetur quis placerat neque lobortis. Nam vestibulum, arcu sodales feugiat consectetur, nisl orci bibendum elit, eu euismod magna sapien ut nibh. Donec semper quam scelerisque tortor dictum gravida. In hac habitasse platea dictumst. Nam pulvinar, odio sed rhoncus suscipit, sem diam ultrices mauris, eu consequat purus metus eu velit. Proin metus odio, aliquam eget molestie nec, gravida ut sapien. Phasellus quis est sed turpis sollicitudin sed eu odio. Praesent eget neque.</p>
 													</section>
 				
 											</section>
-											<section class="col-lg-4 col-md-4 col-sm-6 col-xs-12 executive-committee">
+											<hr class="hidden-lg hidden-md hidden-sm">
+											<section class="col-lg-4 col-md-4 col-sm-12 col-xs-12 executive-committee">
 												<ul class="leadership-photos">
-													<li class="clearfix">
-														<img src="images/vision-leadership/_players/howard-ruby-tb.jpg" alt="Howard Ruby" class="pull-left" />
-														<h3 class="name">Howard Ruby</h3>
-														<p class="title">Chairman, Founder and COO</p>
-														<p><a href="executive-leadership.php" class="orange-link">Full Bio &#8594;</a></p>
+													<div class="row">
+														<section class="col-lg-12 col-md-12 col-sm-6">
+															<li class="clearfix">
+																<img src="images/vision-leadership/_players/howard-ruby-tb.jpg" alt="Howard Ruby" class="pull-left" />
+																<h3 class="name">Howard Ruby</h3>
+																<p class="title">Chairman, Founder and COO</p>
+																<p><a href="executive-leadership.php" class="orange-link">Full Bio &#8594;</a></p>
 												
-													</li>
-													<li class="clearfix">
-														<img src="images/vision-leadership/_players/jill-chapman-tb.jpg" alt="Jill Chapman" class="pull-left" />
-														<h3 class="name">Jill Chapman</h3>
-														<p class="title">SVP, Global Sales and Marketing</p>
-														<p><a href="executive-leadership.php" class="orange-link">Full Bio &#8594;</a></p>
+															</li>
+														</section>
+														<section class="col-lg-12 col-md-12 col-sm-6">
+															<li class="clearfix">
+																<img src="images/vision-leadership/_players/jill-chapman-tb.jpg" alt="Jill Chapman" class="pull-left" />
+																<h3 class="name">Jill Chapman</h3>
+																<p class="title">SVP, Global Sales and Marketing</p>
+																<p><a href="executive-leadership.php" class="orange-link">Full Bio &#8594;</a></p>
 												
-													</li>
-													<li class="clearfix">
-														<img src="images/vision-leadership/_players/marina-lubinsky-tb.jpg" alt="Marina Lubinsky" class="pull-left" />
-														<h3 class="name">Marina Lubinsky</h3>
-														<p class="title">SVP and Chief Information Officer</p>
-														<p><a href="executive-leadership.php" class="orange-link">Full Bio &#8594;</a></p>
+															</li>
+														</section>
+													</div>
+													
+													<div class="row">
+														<section class="col-lg-12 col-md-12 col-sm-6">
+															<li class="clearfix">
+																<img src="images/vision-leadership/_players/marina-lubinsky-tb.jpg" alt="Marina Lubinsky" class="pull-left" />
+																<h3 class="name">Marina Lubinsky</h3>
+																<p class="title">SVP and Chief Information Officer</p>
+																<p><a href="executive-leadership.php" class="orange-link">Full Bio &#8594;</a></p>
 												
-													</li>
-		
+															</li>
+														</section>
 	
 	
-		
-													<li class="clearfix">
-														<img src="images/vision-leadership/_players/ric-villarreal-tb.jpg" alt="Ric Villarreal" class="pull-left" />
-														<h3 class="name">Ric Villarreal</h3>
-														<p class="title">President</p>
-														<p><a href="executive-leadership.php" class="orange-link">Full Bio &#8594;</a></p>
+															<section class="col-lg-12 col-md-12 col-sm-6">	
+																<li class="clearfix">
+																	<img src="images/vision-leadership/_players/ric-villarreal-tb.jpg" alt="Ric Villarreal" class="pull-left" />
+																	<h3 class="name">Ric Villarreal</h3>
+																	<p class="title">President</p>
+																	<p><a href="executive-leadership.php" class="orange-link">Full Bio &#8594;</a></p>
 												
-													</li>
-													<li class="clearfix">
-														<img src="images/vision-leadership/_players/bill-foltz-tb.jpg" alt="Bill Foltz" class="pull-left" />
-														<h3 class="name">Bill Foltz</h3>
-														<p class="title">SVP and Chief Financial Officer</p>
-														<p><a href="executive-leadership.php" class="orange-link">Full Bio &#8594;</a></p>
+																</li>
+															</section>	
+													</div>
+													<div class="row">
+														<section class="col-lg-12 col-md-12 col-sm-6">
+															
+															<li class="clearfix">
+																<img src="images/vision-leadership/_players/bill-foltz-tb.jpg" alt="Bill Foltz" class="pull-left" />
+																<h3 class="name">Bill Foltz</h3>
+																<p class="title">SVP and Chief Financial Officer</p>
+																<p><a href="executive-leadership.php" class="orange-link">Full Bio &#8594;</a></p>
 												
-													</li>
-													<li class="clearfix">
-														<img src="images/vision-leadership/_players/ana-castellanos-tb.jpg" alt="Ana Castellanos" class="pull-left" />
-														<h3 class="name">Ana Castellanos</h3>
-														<p class="title">VP, Chief Human Resources Officer</p>
-														<p><a href="executive-leadership.php" class="orange-link">Full Bio &#8594;</a></p>
+															</li>
+														</section>
+													
+														<section class="col-lg-12 col-md-12 col-sm-6">
+															<li class="clearfix">
+																<img src="images/vision-leadership/_players/ana-castellanos-tb.jpg" alt="Ana Castellanos" class="pull-left" />
+																<h3 class="name">Ana Castellanos</h3>
+																<p class="title">VP, Chief Human Resources Officer</p>
+																<p><a href="executive-leadership.php" class="orange-link">Full Bio &#8594;</a></p>
 												
-													</li>
+															</li>
+														</section>
+													</div>
 												</ul>
 
 											</section>
