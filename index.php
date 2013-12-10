16f225c996843746022971024dcd1b9dd330b07e
Melanie27 <melaniemcganney@gmail.com>
2013-12-10 12:31:05 -0800

    old jquery for all

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
-      <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+     
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
       <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
       
@@ -167,7 +167,7 @@
   		
   	</section><!-- container -->
   	
- 
+  	 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
   	<script src="_/js/bootstrap.js"></script>
   	<script src="_/js/myscript.js"></script>
   </body>
