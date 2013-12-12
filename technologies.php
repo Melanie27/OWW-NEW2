baf925f4b4c92ef8021fb714595467eed0dadec9
Melanie27 <melaniemcganney@gmail.com>
2013-12-12 09:18:47 -0800

    technologies
    desktop, mobile

---
+++ technologies.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>OWW Project</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <!-- Bootstrap -->
+    <link href="_/css/bootstrap.css" rel="stylesheet" media="screen">
+    <link href="_/css/mystyles_global.css" rel="stylesheet" media="screen">
+     <!--<link href="_/css/bootstrap-responsive.min.css" rel="stylesheet" media="screen">-->
+   
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+  
+ 
+   
+  </head>
+  <body id="technologies">
+  <div class="container-full">
+  		<section class="container landing">
+
+		  		<?php include "_/components/php/header.php"; ?>
+		  		
+		  			<section class="two">
+						<div class="row heading">
+							<section class="col-lg-12">
+								<div class="container">
+									<h1>Safety and Security</h1>										
+								</div>
+							</section>		
+						</div><!--heading-->
+							
+						<?php include "_/components/php/hero.php"; ?>
+		  			</section> <!--two container-->	
+						
+											
+						
+							<section class="three container">
+								<div class="row">
+									<section class="col-lg-12 col-md-12  col-sm-12 col-xs-12">
+										<h1>Intro Placeholder Text</h1>
+										<h2>Placeholder. Wherever you need to go, Oakwood Worldwide is there..</h2>
+										
+										<ul>
+											<li> Oakwood Worldwide has developed a number of cutting-edge technologies designed to streamline every aspect of the housing 			program from reservation to departure. </li>
+											<li> Customized tools that provide consistent value in terms of enhancing the comfort of guests and help our clients manage spend, 			streamline processes and make the best business decisions. 
+</li>
+										</ul>
+									</section>
+								</div>
+							</section><!--end three-->
+								<hr>
+								
+							<section class="five container">
+								<div class="row">
+									<section class="col-lg-12 col-md-12 ">
+										<h1>ServiceTrack</h1>
+											<ul class="duty">
+												<li>Our comprehensive, closed-loop issue-tracking system used to document and proactively address guest requests with total transparency.</li>
+												<li>After a service request is made, it is immediately entered into ServiceTrack, time and date stamped and assigned to a Guest Service Representative</li>
+												<li>Each service request is answered within two business hours and OWW strives to have all issues resolved within 24 hours, ensuring your guest is never inconvenienced</li>
+												<li>Guests can receive service ticket status requests in helpful formats such as text or email, and will automatically receive a survey when complete</li>
+												
+											</ul>
+											
+										
+									</section>
+								</div>
+							</section><!--end five-->
+								
+							<hr>
+								
+							<section class="seven container">
+								<div class="row">
+									<section class="col-lg-12 col-md-12 ">
+										<h1>myOakwood and eOakwood</h1>
+										<ul class="crisis-management">
+											<li>myOakwood is a web-based portal for Oakwood guests, offering the ability to view arrival information, view and request changes to their departure date, submit service requests, learn about additional amenities they can obtain for their apartment or residence, obtain maps and driving directions and more.</li>
+											<li>eOakwood is a web-based portal for HR and procurement professionals, allowing them to access the account information of any of their guests; view, reprint, save or email invoices and more.</li>
+										</ul>
+									</section>
+									
+								</div>
+							</section><!--seven-->
+							
+							<hr>
+							
+							<section class="nine container">
+								<div class="row">
+									<section class="col-lg-12 col-md-12">
+										<h1>Epic</h1>
+										<ul class="crisis-management">
+											<li>EPIC is our proprietary e-procurement tool which is ultilized in necessary situations when solutions are required and requested outside of Oakwood Worldwide’s own housing inventory. </li>
+											<li>With over 1,300 suppliers actively participating in EPIC, Oakwood Worldwide is able to leverage choice for its clients and deliver 			optimal housing solutions almost anywhere in the world. </li>
+											
+										</ul>
+									</section>
+									
+								</div>
+							</section><!--nine-->
+							
+							<hr>
+		
+				
+				<?php include "_/components/php/footer.php"; ?>	
+
+  			
+  		
+  	</section><!-- container -->
+  	
+  </div><!--container-full-->
+  	<script src="_/js/bootstrap.js"></script>
+  	<script src="_/js/myscript.js"></script>
+  </body>
+</html>
