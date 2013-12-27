acb8b6267acdda4c5a6e8faaf4d704ada3536baa
Melanie27 <melaniemcganney@gmail.com>
2013-12-27 11:49:44 -0800

    new meta tags for all

--- in-the-news-single.php
+++ in-the-news-single.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-   <title><?php include "_/components/php/title.php"; ?></title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta charset="utf-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+     <title><?php include "_/components/php/title.php"; ?></title>
+     <meta name="description" content="">
+	 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- Bootstrap -->
     <link href="_/css/bootstrap.css" rel="stylesheet" media="screen">
