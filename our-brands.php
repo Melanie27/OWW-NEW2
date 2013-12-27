71e91b1cc746d19ca215a562f129570f552e4c63
Melanie27 <melaniemcganney@gmail.com>
2013-12-27 11:19:59 -0800

    utf

--- our-brands.php
+++ our-brands.php
@@ -3,7 +3,7 @@
   <head>
      <title><?php include "_/components/php/title.php"; ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
+    <meta charset="utf-8" />
 
     <!-- Bootstrap -->
     <link href="_/css/bootstrap.css" rel="stylesheet" media="screen">
