338a39394fc8a747d88f0764324bba6a47fba81b
Melanie27 <melaniemcganney@gmail.com>
2014-01-07 16:56:03 -0800

    awards

--- _/components/php/hero-halves-awards.php
+++ _/components/php/hero-halves-awards.php
@@ -1,62 +1,25 @@
 							<div class="row over hidden-xs container">
-								<section class="col-lg-12 vert">
-									<div class="row">
-										<section class="col-lg-12 col-md-12 col-sm-12 hero-halves">									
-											<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-												<!-- Indicators -->
-												<ol class="carousel-indicators">
-													<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-													<li data-target="#carousel-example-generic" data-slide-to="1"></li>
-													<li data-target="#carousel-example-generic" data-slide-to="2"></li>
-												</ol>
-												<!-- Wrapper for slides -->
-												<div class="carousel-inner">
-													<div class="item active">
-														<section class="row">
-															<div class="col-lg-6 col-md-6 col-sm-6">
-																<img src="images/news/awards/awards-hero-01.jpg" class="img-responsive">
-															</div>																
-															<div class="carousel-caption">
-																<h1>2013</h1>
-																<h2>Cartus Global Network</h2>
-																<p>"Corporate Housing Provider of the Year"</p>
-															</div>	
-														</section>
-													</div>
-													<div class="item">
-														<section class="row">
-															<div class="col-lg-6 col-md-6 col-sm-6">
-																<img src="images/news/awards/awards-hero-01.jpg" class="img-responsive">
-															</div>
-															<div class="carousel-caption">	
-																<h1>1990</h1>
-																<h2>Cartus Global Network</h2>
-																<p>"Corporate Housing Provider of the Year"</p>
-															</div>
-														</section>
-													</div>	
-													<div class="item">
-														<section class="row">
-															<div class="col-lg-6 col-md-6 col-sm-6">
-																<img src="images/news/awards/awards-hero-01.jpg" class="img-responsive">
-															</div>
-															<div class="carousel-caption">	
-																<h1>1980</h1>
-																<h2>Cartus Global Network</h2>
-																<p>"Corporate Housing Provider of the Year"</p>
-															</div>
-														</section>
-													</div>
-												</div><!--end carousel-->
-												<!-- Controls -->
-												<a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
-													<span class="glyphicon glyphicon-chevron-left"></span>
-												</a>
-												<a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
-													<span class="glyphicon glyphicon-chevron-right"></span>
-												</a>
-											</section><!--End hero halves-->	
-										</div><!--end nested row-->	
+								<section class="col-lg-12 vert">						
+									<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">	
+									<!-- Wrapper for slides -->
+										<div class="carousel-inner">
+											<div class="item active">
+												<img src="images/news/awards/awards-slider-01.jpg" >
+											</div>
+											<div class="item">
+												<img src="images/news/awards/awards-slider-01.jpg" class="img-responsive">
+											</div>	
+											<div class="item">
+												<img src="images/news/awards/awards-slider-01.jpg" class="img-responsive">
+											</div>
+										</div><!--end carousel-->
+										<!-- Controls -->
+										<a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+											<span class="glyphicon glyphicon-chevron-left"></span>
+										</a>
+										<a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+											<span class="glyphicon glyphicon-chevron-right"></span>
+										</a>	
 									</section>	<!--vert-->
 								</div><!--row container-->	
 						
